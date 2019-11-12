4604a29804d788fab19123aa3f3598b82fa617b3
Luke Bosse <21162639+lbosse@users.noreply.github.com>
2019-11-12 12:35:12 -0700

    made code more better

--- no-merge.js
+++ no-merge.js
@@ -1,12 +1,10 @@
 var buttons = document.getElementsByTagName("button");
 console.log(buttons);
 var text = "Merge pull request";
-var mergeButton;
 
 for (var i = 0; i < buttons.length; i++) {
   if (buttons[i].innerText == text) {
-    mergeButton = buttons[i];
+    buttons[i].disabled = true;
     break;
   }
 }
-mergeButton.disabled = true;
